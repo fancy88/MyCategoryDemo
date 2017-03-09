3f7e9050884feb091eb6b2d984892427837f89b7
fancy88 <1205090178@qq.com>
2017-03-09 16:33:46 +0800

    添加代码

--- MyCategoryDemo/MyCategoryDemo/ViewController.m
+++ MyCategoryDemo/MyCategoryDemo/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "UIColor+RandomColor.h"
+#import "UIImage+ResizeImage.h"
 
 @interface ViewController ()
 
@@ -17,6 +19,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self getUI];
+}
+
+- (void)getUI{
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(self.view.frame.size.width / 2.0 - 50, 200, 100, 40);
+    button.backgroundColor = [UIColor cyanColor];
+    [button setTitle:@"Click" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
+    UIImageView *ImgView = [[UIImageView alloc] initWithFrame:CGRectMake(self.view.frame.size.width / 2.0 - 50, 300, 100, 100)];
+    [ImgView setImage:[UIImage imageWithString:@"iceberg.jpeg"]];
+    [self.view addSubview:ImgView];
+    
+}
+
+- (void)buttonAction: (UIButton *)button{
+    button.backgroundColor = [UIColor RandomColor];
+    NSLog(@"Hello world!");
 }
 
 
